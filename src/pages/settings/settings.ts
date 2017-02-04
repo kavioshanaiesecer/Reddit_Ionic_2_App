3f8ccae1e4d74baf2960a6922ea5e76bad393ffa
kavioshanaiesecer <kavi.oshan8@gmail.com>
2017-02-04 18:51:56 +0530

    settings view completed

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-
+import {RedditsSevice} from '../../app/services/reddits.service';
 import { NavController } from 'ionic-angular';
+import {DetailsPage} from '../details/details';
 
 @Component({
   selector: 'settings',
   templateUrl: 'settings.html'
 })
 export class SettingsPage {
-
-  constructor(public navCtrl: NavController) {
-
+  
+  catergory:any;
+  limit :any;
+  constructor(public navCtrl: NavController,private redditService:RedditsSevice) {
+      this.getDefaults();
   }
+
+
+getDefaults(){
+  this.catergory="sports";
+  this.limit = 10;
+}
 
 }
